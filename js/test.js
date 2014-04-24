2ff2ed4abca47ea815c42bb793336a072a4adbd9
maeponyo <maeponyo@gmail.com>
2014-04-24 11:56:28 +0900

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,22 @@
+var output2 = document.getElementById("output2");
+var button = documetn.getElementById("fizbiz");
+
+var fizbiz= function(){
+	var a = 1;
+	while(a < 30){
+	if (a % 15 = 0){
+		output2 = "FizBiz" +<br>; 
+	}
+	else if(a % 5 = 0){
+		output2 = "Biz" ;
+	}
+	else if (a % 3 = 0){
+		output2 = "Fiz";
+	}
+	else{
+		output2 = a ;
+	}
+	document.write = output2;
+	a = a +1 ;
+	}
+}
