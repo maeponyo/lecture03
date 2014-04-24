db80a7c67527036b81042ed21c15de50e828923d
maeponyo <maeponyo@gmail.com>
2014-04-24 11:10:03 +0900

    練習④できたよ!!!!

--- js/input-output.js
+++ js/input-output.js
@@ -1,6 +1,7 @@
 var input = document.querySelector("#input");
 var output = document.querySelector("#output");
 var registerMessageButton = document.querySelector("#registerMessage");
+var clerMessageButton = document.querySelector("#clearMessage");
 
 var registerMessage = function(){
 	var message = document.createElement("li");
@@ -9,4 +10,11 @@ var registerMessage = function(){
 	output.appendChild(message);
 };
 
+var clearMessage = function(){
+	while(output.firstChild){
+output.removeChild(output.firstChild);
+}
+}
+
 registerMessageButton.addEventListener("click",registerMessage);
+clerMessageButton.addEventListener("click",clearMessage);
