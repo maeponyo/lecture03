d13b0888d2cbffcbd090999fcf8b5cd67474f564
maeponyo <maeponyo@gmail.com>
2014-04-24 10:46:09 +0900

    練習2まで終了

--- js/display-message.js
+++ js/display-message.js
@@ -10,3 +10,33 @@ var showInfoMessages = function(){
 
 var showInfoMessagesButton = document.getElementById("showInfoMessages");
 showInfoMessagesButton.addEventListener("click", showInfoMessages);
+
+
+//練習問題3－2
+var showWarningMessages = function(){
+	var messages = document.querySelectorAll(".warning");
+
+	var i = 0;
+	while(i < messages.length){
+		messages[i].setAttribute("class", "warning");
+		i = i + 1;
+	}
+};
+
+var showWarningMessagesButton = document.getElementById("showWarningMessages");
+showWarningMessagesButton.addEventListener("click", showWarningMessages);
+
+
+//練習問題3-3
+var showErrorMessages = function(){
+	var messages = document.querySelectorAll(".error");
+
+	var i = 0;
+	while(i < messages.length){
+		messages[i].setAttribute("class", "error");
+		i = i + 1;
+	}
+};
+
+var showErrorMessagesButton = document.getElementById("showErrorMessages");
+showErrorMessagesButton.addEventListener("click", showErrorMessages);
