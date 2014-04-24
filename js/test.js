fa3789fe351ffeb10c4f93fe1588b4d334430a79
maeponyo <maeponyo@gmail.com>
2014-04-24 13:09:28 +0900

    ボタンを押すと表示できるようになったよ!!

--- js/test.js
+++ js/test.js
@@ -1,11 +1,14 @@
+function go(){
 var a = 0;
 var b = 1;
 var c;
 var out = document.getElementById("out");
-out.innerHTML += a + "<br>" + b; 
+out.innerHTML += b; 
 
 while (b <= 1000){
 var c = a + b;
   out.innerHTML += "<br>" + c;
   a = b;
   b = c;}
+
+}
