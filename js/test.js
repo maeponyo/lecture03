6a3e9b8ec59a2e9c12ae705019ff6c2acee6ab43
maeponyo <maeponyo@gmail.com>
2014-04-24 12:27:02 +0900

    新しいファイルつくれたよ!!!!
    フィボナッチ数列なうｗ(ボタンなし)

--- js/test.js
+++ js/test.js
@@ -1,22 +1,11 @@
-var output2 = document.getElementById("output2");
-var button = documetn.getElementById("fizbiz");
+var a = 0;
+var b = 1;
+var c;
+var out = document.getElementById("out");
+out.innerHTML += a + "<br>" + b; 
 
-var fizbiz= function(){
-	var a = 1;
-	while(a < 30){
-	if (a % 15 = 0){
-		output2 = "FizBiz" +<br>; 
-	}
-	else if(a % 5 = 0){
-		output2 = "Biz" ;
-	}
-	else if (a % 3 = 0){
-		output2 = "Fiz";
-	}
-	else{
-		output2 = a ;
-	}
-	document.write = output2;
-	a = a +1 ;
-	}
-}
+while (b <= 1000){
+var c = a + b;
+  out.innerHTML += "<br>" + c;
+  a = b;
+  b = c;}
